da1d489534ec349d5ee9e4f0a405c430c904727f
alex <reborn9226@gmail.com>
2026-02-15 20:54:32 +0300

    Создал две папки для лабы Терраформ 2

---
+++ terraform/lab2/variables.tf
@@ -0,0 +1,37 @@
+variable "string" {
+    type=string
+    default="какая-то строка"
+}
+variable "number" {
+    type=number
+    default="1"
+}
+variable "list_of_strings" {
+    type=list(string)
+    default=["a","b","c"]
+}
+variable "lost_of_numbers" {
+    type=list(number)
+    default=[1,2,3]
+}
+variable "map" {
+    type=map(string)
+    default={
+        name="Alexandr"
+        surname="Ershov"
+    }
+}
+variable "bool" {
+    type = bool
+    default = true
+}
+#variable "containers" {
+#    type = map(object({
+#      name = string
+#      image = string
+#      port = object ({
+#        external = number
+#        internal = number
+#      })
+#    }))
+#}
