1498b0b436dca66309d77536f030101964bc2f5f
alex <reborn9226@gmail.com>
2026-08-04 09:48:43 +0300

    Исправил опечатку

--- kursovoy/output.tf
+++ kursovoy/output.tf
@@ -13,7 +13,7 @@ output "vms_info" {
 }
 
 #  Вывод публичного IP балансировщика
-output "alb_public_ip" {
+output "web_balancer" {
   description = "Публичный IP-адрес балансировщика ALB"
-  value = yandex_alb_load_balancer.web_alb.listener[0].endpoint[0].address[0].external_ipv4_address[0].address
+  value = yandex_alb_load_balancer.web_balancer.listener[0].endpoint[0].address[0].external_ipv4_address[0].address
 }
